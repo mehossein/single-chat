0740369fee8a47be8e0f75f336e87a96ad4a3239
hossein khoshyari <hossein12khoshyari@gmail.com>
2022-03-07 15:02:10 +0330

    [feat] done chat component html

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,8 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './component/chat/chat.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: ChatComponent,
+      },
+    ],
+  },
   {
     path: '**',
     redirectTo: '',
@@ -11,7 +21,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [],
+  declarations: [ChatComponent],
   imports: [
     CommonModule,
     SharedModule,
--- src/app/main/models/rows.ts
+++
@@ -1,8 +0,0 @@
-export interface Rows {
-  name: string;
-  type: string;
-  label: string;
-  nullable: boolean;
-  readonly: boolean;
-  editMode?: boolean;
-}
